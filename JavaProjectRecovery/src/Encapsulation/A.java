f48c7836912e2f62221ddb36ed11b687bcf722bd
yasminghahramani <yasminghahramani@yasmins-air>
2023-09-01 11:02:19 -0400

    Encapsulation.

---
+++ JavaProjectRecovery/src/Encapsulation/A.java
@@ -0,0 +1,36 @@
+package Encapsulation;
+
+public class A {
+	
+
+private static int empID; 
+
+private static int salary; 
+	
+public static void setempID(int a) {
+	empID = a;
+}
+
+public static void setsalary(int b) {
+	salary = b;
+}
+	
+public static int getempID() {
+	return empID;
+}
+
+public static int getsalary() {
+	return salary; 
+} 
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+		
+		
+	}
+
+}
---
+++ JavaProjectRecovery/src/Encapsulation/B.java
@@ -0,0 +1,19 @@
+package Encapsulation;
+
+public class B extends A {
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		setempID(334455); 
+		
+		setsalary(6250);
+		
+		System.out.println(getempID());  
+		
+		System.out.println(getsalary());  
+		
+	}
+
+}
