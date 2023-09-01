7ec5e0aa101148db7089c12508e8336037513a0f
yasminghahramani <yasminghahramani@yasmins-air>
2023-09-01 11:02:40 -0400

    Oops Abstraction 2.

---
+++ JavaProjectRecovery/src/OopsAbstraction2/B.java
@@ -0,0 +1,21 @@
+package OopsAbstraction2;
+
+public interface B {
+	
+	void beuatyProducts();
+	
+	void clothing(); 
+	
+	void toys();
+	
+	void kitchen();
+	
+	void furniture();
+	
+	void outdoorProducts();
+	
+	void electronics();
+	
+	
+	
+}
