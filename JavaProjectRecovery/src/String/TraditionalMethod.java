71834c6f4a26c4936d53a20b6ee19e9e2d462aab
yasminghahramani <yasminghahramani@yasmins-air>
2023-09-08 16:07:10 -0400

    Traditional Method to define string.

---
+++ JavaProjectRecovery/src/String/TraditionalMethod.java
@@ -0,0 +1,24 @@
+package String;
+
+public class TraditionalMethod {
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// This is how to create a string in traditional way
+		String a = new String("Yasmin"); 
+		System.out.println(a);
+		
+		String b = new String("Tom");
+		System.out.println(b);
+		
+		String c = new String ("Yasmin");
+		System.out.println(c);
+		
+		String d = new String("yasmin"); 
+		System.out.println(d);
+		
+	}
+
+}
