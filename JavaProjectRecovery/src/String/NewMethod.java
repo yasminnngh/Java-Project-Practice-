448223e9f126e88e57e2ed0986857423088afa9f
yasminghahramani <yasminghahramani@yasmins-air>
2023-09-08 16:10:08 -0400

    New Method.

---
+++ JavaProjectRecovery/src/String/NewMethod.java
@@ -0,0 +1,22 @@
+package String;
+
+public class NewMethod {
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String a = "Yasmin"; 
+		System.out.println(a);
+		
+		String b = "Yasmin"; 
+		System.out.println(b);
+		
+		String c = "yasmin";
+		System.out.println(c);
+		
+		String d = "YASMIN"; 
+		System.out.println(d);
+	}
+
+}
