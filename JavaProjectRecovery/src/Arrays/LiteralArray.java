6b6f45af6d928d09e263221384c07b39d4d89bb7
yasminghahramani <yasminghahramani@yasmins-air>
2023-09-08 16:10:53 -0400

    Literal Array.

---
+++ JavaProjectRecovery/src/Arrays/LiteralArray.java
@@ -0,0 +1,34 @@
+package Arrays;
+
+public class LiteralArray {
+
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int[] a = {100, 200, 300, 400, 500};
+		
+		
+//		for(int i = 0; i < a.length; i++) {
+//			
+//			System.out.println(a[i]);
+//			
+//		}
+		
+//		for(int number: a ) {
+//			
+//			System.out.println(number);
+//			
+//		}
+		
+		for(int i = a.length -1; i >= 0; i--) {
+			
+			System.out.println(a[i]);
+			
+		}
+		
+		
+	}
+
+}
